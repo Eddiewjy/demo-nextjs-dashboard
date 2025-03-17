d5072fffb71bc48490938f7a827c18084ef1c6c8
EddieWjy <3040953216@qq.com>
2025-03-17 11:08:53 +0800

    配置 ESLint

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
   },
+  eslint: {
+    ignoreDuringBuilds: true, // 让 ESLint 在构建时不阻止编译
+  },
 };
 
 export default nextConfig;
